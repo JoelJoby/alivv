d506509028465067ec3c9d96e94a7be35900c7d4
Joel Joby <72356289+JoelJoby@users.noreply.github.com>
2025-12-06 15:16:56 +0530

    Update settings.py

--- alivv/settings.py
+++ alivv/settings.py
@@ -24,15 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&kld(=^yx_yqdo8anf^wbk8vfj8arol!v%fm^blruir)aqbri9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [
-    'alivv-prod.eba-jpcsyyzz.ap-south-1.elasticbeanstalk.com',
-    'alivv-prod.eba-jpcsypzz.ap-south-1.elasticbeanstalk.com',  # your exact CNAME
-    '.elasticbeanstalk.com',
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,13 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
