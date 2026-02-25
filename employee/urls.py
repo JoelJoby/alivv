06e48eb259a7df90696dfb98beefd578f7e1d23d
JoelJoby <joeljoby369@gmail.com>
2026-02-25 22:28:36 +0530

    Added employee module

---
+++ employee/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.staff_login, name='staff_login'),
+    path('logout/', views.staff_logout, name='staff_logout'),
+    path('dashboard/', views.staff_dashboard, name='staff_dashboard'),
+    path('orders/', views.staff_orders, name='staff_orders'),
+]
